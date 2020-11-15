03d31b81025488ab50ad837839aa75cccc59ee93
vladstojna <vlad.leic@gmail.com>
2020-11-15 18:48:59 +0000

    add base energy_reader class

---
+++ src/energy_reader.h
@@ -0,0 +1,43 @@
+// energy_reader.h
+#pragma once
+
+#include <iosfwd>
+#include <stdexcept>
+
+namespace tep
+{
+
+constexpr const size_t ISAMPLE_SIZE = 128;
+
+class energy_reader_exception : public std::runtime_error
+{
+public:
+    energy_reader_exception(const char* what) :
+        std::runtime_error(what) {}
+    energy_reader_exception(const std::string& what) :
+        std::runtime_error(what) {}
+};
+
+class energy_reader
+{
+protected:
+    uint64_t _sample_count = 0;
+
+public:
+    virtual ~energy_reader() = default;
+    virtual void start() = 0;
+    virtual void sample() = 0;
+    virtual void stop() = 0;
+
+    friend std::ostream& operator<<(std::ostream& os, const energy_reader& er)
+    {
+        er.print(os);
+        return os;
+    }
+
+protected:
+    // count,timestamp,skt0_cpu,skt0_dram,...
+    virtual void print(std::ostream& os) const = 0;
+};
+
+}
