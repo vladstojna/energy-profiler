12c80eab6957953b7975ddfcc9b0f8817330ab58
vladstojna <vlad.leic@gmail.com>
2021-03-12 03:18:48 +0000

    add support for scalar types in scuffed expected

--- util/expected.hpp
+++ util/expected.hpp
@@ -21,11 +21,23 @@ namespace cmmn
         static_assert(!std::is_reference_v<R>, "Result must be a value type");
         static_assert(!std::is_void_v<E>, "Result cannot be void");
         static_assert(!std::is_reference_v<E>, "Result must be a value type");
+        static_assert(!std::is_same_v<R, E>, "Result and Error must be different types");
 
     private:
         std::variant<E, R> _result;
 
     public:
+
+        template<typename T = E, std::enable_if_t<std::is_scalar_v<T>, bool> = true>
+        expected(E error) :
+            _result(error)
+        {
+            DBG(std::cout << "[result@"
+                << reinterpret_cast<uintptr_t>(this)
+                << ": value error]\n");
+        }
+
+        template<typename T = E, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         expected(const E& error) :
             _result(error)
         {
@@ -34,6 +46,7 @@ namespace cmmn
                 << ": const& error]\n");
         }
 
+        template<typename T = E, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         expected(E&& error) :
             _result(std::move(error))
         {
@@ -42,6 +55,16 @@ namespace cmmn
                 << ": && error]\n");
         }
 
+        template<typename T = R, std::enable_if_t<std::is_scalar_v<T>, bool> = true>
+        expected(R result) :
+            _result(result)
+        {
+            DBG(std::cout << "[result@"
+                << reinterpret_cast<uintptr_t>(this)
+                << ": value result]\n");
+        }
+
+        template<typename T = R, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         expected(const R& result) :
             _result(result)
         {
@@ -50,6 +73,7 @@ namespace cmmn
                 << ": const& result]\n");
         }
 
+        template<typename T = R, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         expected(R&& result) :
             _result(std::move(result))
         {
@@ -58,7 +82,7 @@ namespace cmmn
                 << ": && result]\n");
         }
 
-        template<typename... Args>
+        template<typename... Args, typename T = R, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         expected(Args&&... args) :
             _result(std::in_place_type<R>, std::forward<Args>(args)...)
         {
@@ -67,17 +91,35 @@ namespace cmmn
                 << ": result constructed in place]\n");
         }
 
-        const E& error() const
+        template<typename T = E, std::enable_if_t<std::is_scalar_v<T>, bool> = true>
+        E error() const
         {
+            DBG(std::cout << "[error: returned by value]\n");
             return std::get<E>(_result);
         }
 
+        template<typename T = E, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
+        const E& error() const
+        {
+            DBG(std::cout << "[error: returned const&]\n");
+            return std::get<E>(_result);
+        }
+
+        template<typename T = R, std::enable_if_t<std::is_scalar_v<T>, bool> = true>
+        R value()
+        {
+            DBG(std::cout << "[result: returned by value]\n");
+            return std::get<R>(_result);
+        }
+
+        template<typename T = R, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         const R& value() const
         {
             DBG(std::cout << "[result: returned by const&]\n");
             return std::get<R>(_result);
         }
 
+        template<typename T = R, std::enable_if_t<!std::is_scalar_v<T>, bool> = true>
         R& value()
         {
             DBG(std::cout << "[result: returned by &]\n");
