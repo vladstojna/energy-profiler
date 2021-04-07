a92fe9f074ce295cdae321c36cea53e15e8d8060
vladstojna <vlad.leic@gmail.com>
2021-04-07 18:23:00 +0100

    simple post-processed print

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // main.cpp
 
 #include <cstring>
-#include <fstream>
+#include <functional>
 #include <iostream>
 
 #include "cmdargs.hpp"
@@ -11,6 +11,130 @@
 #include "ptrace_wrapper.hpp"
 #include "target.hpp"
 #include "util.hpp"
+
+namespace tep
+{
+
+    template<typename T>
+    class rdr_task_pair
+    {
+    private:
+        const T* _reader;
+        const nrgprf::task* _task;
+
+    public:
+        rdr_task_pair(const T& reader, const nrgprf::task& task) :
+            _reader(&reader),
+            _task(&task)
+        {}
+
+        const T& reader() const { return *_reader; }
+        const nrgprf::task& task() const { return *_task; }
+    };
+
+
+    std::ostream& write_cpu_energy(std::ostream& os,
+        const std::function<nrgprf::result<uint64_t>(const nrgprf::sample&, uint8_t skt)>& func,
+        const nrgprf::sample& first, const nrgprf::sample& last, uint8_t skt)
+    {
+        nrgprf::result<uint64_t> res_first = func(first, skt);
+        nrgprf::result<uint64_t> res_last = func(last, skt);
+
+        if (res_first && res_last)
+            os << ", " << (res_last.value() - res_first.value()) * 1e-6 << " J";
+        else
+            os << ", N/A";
+        return os;
+    }
+
+
+    std::ostream& operator<<(std::ostream& os, const rdr_task_pair<nrgprf::reader_gpu>& rt)
+    {
+        for (size_t ix = 0; ix < rt.task().size(); ix++)
+        {
+            const nrgprf::execution& exec = rt.task().get(ix);
+            os << ix << " | ";
+            os << (exec.last() - exec.first()).count() * 1e-9 << " s |";
+
+            for (uint8_t dev = 0; dev < nrgprf::MAX_SOCKETS; dev++)
+            {
+                double total_energy = 0.0;
+                for (size_t s = 1; s < exec.size(); s++)
+                {
+                    const nrgprf::sample& prev = exec.get(s - 1);
+                    const nrgprf::sample& curr = exec.get(s);
+
+                    os << " dev=" << +dev;
+                    nrgprf::result<uint64_t> pwr_prev = rt.reader().get_board_power(prev, dev);
+                    nrgprf::result<uint64_t> pwr_curr = rt.reader().get_board_power(curr, dev);
+                    if (pwr_prev && pwr_curr)
+                    {
+                        // mW * nS = W * 1e3 * s * 1e9 = J * 1e12
+                        nrgprf::duration_t dur = curr - prev;
+                        total_energy += (pwr_prev.value() + pwr_curr.value()) / 2.0 * dur.count() * 1e-12;
+                    }
+                    else
+                        break;
+                }
+                if (total_energy == 0.0)
+                    os << ", N/A";
+                else
+                    os << total_energy << " J";
+            }
+        }
+        return os;
+    }
+
+
+    std::ostream& operator<<(std::ostream& os, const rdr_task_pair<nrgprf::reader_rapl>& rt)
+    {
+        for (size_t ix = 0; ix < rt.task().size(); ix++)
+        {
+            const nrgprf::execution& exec = rt.task().get(ix);
+            const nrgprf::sample& sfirst = exec.first();
+            const nrgprf::sample& slast = exec.last();
+
+            os << ix << " | ";
+            os << (slast - sfirst).count() * 1e-9 << " s |";
+            for (uint8_t skt = 0; skt < nrgprf::MAX_SOCKETS; skt++)
+            {
+                using namespace std::placeholders;
+                os << " skt=" << +skt;
+                write_cpu_energy(os, std::bind(&nrgprf::reader_rapl::get_pkg_energy, &rt.reader(), _1, _2), sfirst, slast, skt);
+                write_cpu_energy(os, std::bind(&nrgprf::reader_rapl::get_pp0_energy, &rt.reader(), _1, _2), sfirst, slast, skt);
+                write_cpu_energy(os, std::bind(&nrgprf::reader_rapl::get_pp1_energy, &rt.reader(), _1, _2), sfirst, slast, skt);
+                write_cpu_energy(os, std::bind(&nrgprf::reader_rapl::get_dram_energy, &rt.reader(), _1, _2), sfirst, slast, skt);
+                os << " |";
+            }
+            os << "\n";
+        }
+        return os;
+    }
+
+
+    std::ostream& operator<<(std::ostream& os, const profiling_results& pr)
+    {
+        os << "# Results\n";
+        for (const auto& sr : pr.results)
+        {
+            os << "# Begin section\n";
+            os << sr.section << "\n";
+            os << "# End section\n";
+            os << "# Begin readings\n";
+            switch (sr.section.target())
+            {
+            case config_data::target::cpu:
+                os << rdr_task_pair(pr.rdr_cpu, sr.readings);
+                break;
+            case config_data::target::gpu:
+                os << rdr_task_pair(pr.rdr_gpu, sr.readings);
+                break;
+            }
+            os << "# End readings\n";
+        }
+        return os;
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -50,6 +174,7 @@ int main(int argc, char* argv[])
             std::cerr << results.error() << std::endl;
             return 1;
         }
+        args.value().outfile().stream() << results.value();
         return 0;
     }
     else if (child_pid == -1)
