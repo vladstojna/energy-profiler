845ed300a579adbd8a150cd4531799ba97de2cd8
vladstojna <vlad.leic@gmail.com>
2021-04-14 03:39:29 +0100

    add common reader unit types

---
+++ nrg/src/reader_units.hpp
@@ -0,0 +1,13 @@
+// reader_units.hpp
+
+#pragma once
+
+#include "units.hpp"
+
+namespace nrgprf
+{
+
+    using units_energy = microjoules<uint64_t>;
+    using units_power = microwatts<uint64_t>;
+
+}
