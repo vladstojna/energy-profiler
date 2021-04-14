39fdb72963064710cf0ba58479ada44c2b56821d
vladstojna <vlad.leic@gmail.com>
2021-04-14 20:40:58 +0100

    change to uintmax

--- nrg/src/sample.hpp
+++ nrg/src/sample.hpp
@@ -11,14 +11,14 @@ namespace nrgprf
     class sample : public basic_sample
     {
     private:
-        uint64_t _values[MAX_SOCKETS * MAX_RAPL_DOMAINS + MAX_SOCKETS];
+        uintmax_t _values[MAX_SOCKETS * MAX_RAPL_DOMAINS + MAX_SOCKETS];
 
     public:
         sample(const timepoint_t& tp) : basic_sample(tp) {}
         sample(timepoint_t&& tp) : basic_sample(std::move(tp)) {}
 
-        uint64_t get(size_t idx) const { return _values[idx]; }
-        void set(size_t idx, uint64_t val) { _values[idx] = val; }
+        uintmax_t get(size_t idx) const { return _values[idx]; }
+        void set(size_t idx, uintmax_t val) { _values[idx] = val; }
     };
 
 }
