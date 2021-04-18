6ef1dad9b39b3fc3aaffcd58fedad3c444281cb0
vladstojna <vlad.leic@gmail.com>
2021-04-19 00:59:41 +0100

    move reader creation to profiler constructor

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -50,39 +50,87 @@ tracer_error handle_reader_error(pid_t pid, const nrgprf::error& error)
     return { tracer_errcode::READER_ERROR, error.msg() };
 }
 
+
+nrgprf::rapl_domain get_rapl_domain_mask(const config_data& cd)
+{
+    return static_cast<nrgprf::rapl_domain>(cd.parameters().domain_mask() & 0xff);
+}
+
+uint8_t get_socket_mask(const config_data& cd)
+{
+    return static_cast<uint8_t>(cd.parameters().socket_mask() & 0xff);
+}
+
+uint8_t get_device_mask(const config_data& cd)
+{
+    return static_cast<uint8_t>(cd.parameters().device_mask() & 0xff);
+}
+
+nrgprf::reader_rapl create_cpu_reader(pid_t tid, const config_data& cd, tracer_error& err)
+{
+    nrgprf::error error = nrgprf::error::success();
+    nrgprf::reader_rapl reader(get_rapl_domain_mask(cd), get_socket_mask(cd), error);
+    if (error)
+        err = handle_reader_error(tid, error);
+    log(log_lvl::success, "[%d] created RAPL reader", tid);
+    return reader;
+}
+
+nrgprf::reader_gpu create_gpu_reader(pid_t tid, const config_data& cd, tracer_error& err)
+{
+    nrgprf::error error = nrgprf::error::success();
+    nrgprf::reader_gpu reader(get_device_mask(cd), error);
+    if (error)
+        err = handle_reader_error(tid, error);
+    log(log_lvl::success, "[%d] created GPU reader", tid);
+    return reader;
+}
+
 // end helper functions
 
 
-profiler::profiler(pid_t child, bool pie, const dbg_line_info& dli, const config_data& cd) :
+profiler::profiler(pid_t child, bool pie,
+    const dbg_line_info& dli, const config_data& cd, tracer_error& err) :
+    _tid(gettid()),
     _child(child),
     _pie(pie),
     _dli(dli),
     _cd(cd),
-    _traps()
+    _rdr_cpu(create_cpu_reader(_tid, cd, err)),
+    _rdr_gpu(create_gpu_reader(_tid, cd, err))
 {}
 
-profiler::profiler(pid_t child, bool pie, const dbg_line_info& dli, config_data&& cd) :
+profiler::profiler(pid_t child, bool pie,
+    const dbg_line_info& dli, config_data&& cd, tracer_error& err) :
+    _tid(gettid()),
     _child(child),
     _pie(pie),
     _dli(dli),
     _cd(std::move(cd)),
-    _traps()
+    _rdr_cpu(create_cpu_reader(_tid, cd, err)),
+    _rdr_gpu(create_gpu_reader(_tid, cd, err))
 {}
 
-profiler::profiler(pid_t child, bool pie, dbg_line_info&& dli, const config_data& cd) :
+profiler::profiler(pid_t child, bool pie,
+    dbg_line_info&& dli, const config_data& cd, tracer_error& err) :
+    _tid(gettid()),
     _child(child),
     _pie(pie),
     _dli(std::move(dli)),
     _cd(cd),
-    _traps()
+    _rdr_cpu(create_cpu_reader(_tid, cd, err)),
+    _rdr_gpu(create_gpu_reader(_tid, cd, err))
 {}
 
-profiler::profiler(pid_t child, bool pie, dbg_line_info&& dli, config_data&& cd) :
+profiler::profiler(pid_t child, bool pie,
+    dbg_line_info&& dli, config_data&& cd, tracer_error& err) :
+    _tid(gettid()),
     _child(child),
     _pie(pie),
     _dli(std::move(dli)),
     _cd(std::move(cd)),
-    _traps()
+    _rdr_cpu(create_cpu_reader(_tid, cd, err)),
+    _rdr_gpu(create_gpu_reader(_tid, cd, err))
 {}
 
 const dbg_line_info& profiler::debug_line_info() const
@@ -103,18 +151,16 @@ const trap_set& profiler::traps() const
 tracer_expected<profiling_results> profiler::run()
 {
     int wait_status;
-    int tid = gettid();
-
     pid_t waited_pid = waitpid(_child, &wait_status, 0);
     if (waited_pid == -1)
-        return get_syserror(errno, tracer_errcode::SYSTEM_ERROR, tid, "waitpid");
+        return get_syserror(errno, tracer_errcode::SYSTEM_ERROR, _tid, "waitpid");
     assert(waited_pid == _child);
 
-    log(log_lvl::info, "[%d] started the profiling procedure for child %d", tid, waited_pid);
+    log(log_lvl::info, "[%d] started the profiling procedure for child %d", _tid, waited_pid);
 
     if (!WIFSTOPPED(wait_status))
     {
-        log(log_lvl::error, "[%d] ptrace(PTRACE_TRACEME, ...) called but target was not stopped", tid);
+        log(log_lvl::error, "[%d] ptrace(PTRACE_TRACEME, ...) called but target was not stopped", _tid);
         return tracer_error(tracer_errcode::PTRACE_ERROR,
             "Tracee not stopped despite being attached with ptrace");
     }
@@ -123,23 +169,23 @@ tracer_expected<profiling_results> profiler::run()
     ptrace_wrapper& pw = ptrace_wrapper::instance;
     user_regs_struct regs;
     if (pw.ptrace(errnum, PTRACE_GETREGS, waited_pid, 0, &regs) == -1)
-        return get_syserror(errnum, tracer_errcode::PTRACE_ERROR, tid, "PTRACE_GETREGS");
+        return get_syserror(errnum, tracer_errcode::PTRACE_ERROR, _tid, "PTRACE_GETREGS");
 
     uintptr_t entrypoint;
     if (get_entrypoint_addr(_pie, _child, entrypoint) == -1)
-        return get_syserror(errno, tracer_errcode::SYSTEM_ERROR, tid, "get_entrypoint_addr");
+        return get_syserror(errno, tracer_errcode::SYSTEM_ERROR, _tid, "get_entrypoint_addr");
 
     log(log_lvl::info, "[%d] tracee %d rip @ 0x%" PRIxPTR ", entrypoint @ 0x%" PRIxPTR,
-        tid, waited_pid, get_ip(regs), entrypoint);
+        _tid, waited_pid, get_ip(regs), entrypoint);
 
     if (pw.ptrace(errnum, PTRACE_SETOPTIONS, waited_pid, 0, get_ptrace_opts(true)) == -1)
-        return get_syserror(errnum, tracer_errcode::PTRACE_ERROR, tid, "PTRACE_SETOPTIONS");
-    log(log_lvl::debug, "[%d] ptrace options successfully set", tid);
+        return get_syserror(errnum, tracer_errcode::PTRACE_ERROR, _tid, "PTRACE_SETOPTIONS");
+    log(log_lvl::debug, "[%d] ptrace options successfully set", _tid);
 
     // iterate the sections defined in the config and insert their respective breakpoints
     if (!_dli.has_dbg_symbols())
     {
-        log(log_lvl::error, "[%d] no debugging information found", tid);
+        log(log_lvl::error, "[%d] no debugging information found", _tid);
         return tracer_error(tracer_errcode::NO_SYMBOL, "No debugging information found");
     }
 
@@ -152,70 +198,56 @@ tracer_expected<profiling_results> profiler::run()
         if (!start_cu)
         {
             log(log_lvl::error, "[%d] start compilation unit: %s",
-                tid, start_cu.error().message.c_str());
+                _tid, start_cu.error().message.c_str());
             return tracer_error(tracer_errcode::NO_SYMBOL, std::move(start_cu.error().message));
         }
         auto end_cu = _dli.find_cu(end.compilation_unit());
         if (!end_cu)
         {
             log(log_lvl::error, "[%d] end compilation unit: %s",
-                tid, end_cu.error().message.c_str());
+                _tid, end_cu.error().message.c_str());
             return tracer_error(tracer_errcode::NO_SYMBOL, std::move(end_cu.error().message));
         }
 
         auto start_offset = start_cu.value()->line_first_addr(start.line());
         if (!start_offset)
         {
-            log(log_lvl::error, "[%d] start compilation unit: invalid line %" PRIu32, tid, start.line());
+            log(log_lvl::error, "[%d] start compilation unit: invalid line %" PRIu32, _tid, start.line());
             return tracer_error(tracer_errcode::NO_SYMBOL, std::move(start_offset.error().message));
         }
         auto end_offset = end_cu.value()->line_first_addr(end.line());
         if (!end_offset)
         {
-            log(log_lvl::error, "[%d] start compilation unit: invalid line %" PRIu32, tid, end.line());
+            log(log_lvl::error, "[%d] start compilation unit: invalid line %" PRIu32, _tid, end.line());
             return tracer_error(tracer_errcode::NO_SYMBOL, std::move(end_offset.error().message));
         }
 
-        log(log_lvl::debug, "[%d] start offset 0x%" PRIxPTR, tid, start_offset.value());
-        log(log_lvl::debug, "[%d] end offset 0x%" PRIxPTR, tid, end_offset.value());
+        log(log_lvl::debug, "[%d] start offset 0x%" PRIxPTR, _tid, start_offset.value());
+        log(log_lvl::debug, "[%d] end offset 0x%" PRIxPTR, _tid, end_offset.value());
 
         uintptr_t start_addr = entrypoint + start_offset.value();
         uintptr_t end_addr = entrypoint + end_offset.value();
-        auto orig_word_start = insert_trap(tid, waited_pid, start_addr);
+        auto orig_word_start = insert_trap(_tid, waited_pid, start_addr);
         if (!orig_word_start)
             return std::move(orig_word_start.error());
-        auto orig_word_end = insert_trap(tid, waited_pid, end_addr);
+        auto orig_word_end = insert_trap(_tid, waited_pid, end_addr);
         if (!orig_word_end)
             return std::move(orig_word_end.error());
         _traps.emplace(start_addr, orig_word_start.value(), sec);
         log(log_lvl::info, "[%d] inserted trap @ 0x%" PRIxPTR " (offset 0x%" PRIxPTR ")",
-            tid, start_addr, start_addr - entrypoint);
+            _tid, start_addr, start_addr - entrypoint);
         _traps.emplace(end_addr, orig_word_end.value(), sec);
         log(log_lvl::info, "[%d] inserted trap @ 0x%" PRIxPTR " (offset 0x%" PRIxPTR ")",
-            tid, end_addr, end_addr - entrypoint);
+            _tid, end_addr, end_addr - entrypoint);
     }
 
-    // create readers
-    log(log_lvl::debug, "[%d] params: 0x%x 0x%x 0x%x", tid, _cd.parameters().domain_mask(),
-        _cd.parameters().socket_mask(), _cd.parameters().device_mask());
-    nrgprf::error error = nrgprf::error::success();
-    nrgprf::reader_rapl rdr_cpu(static_cast<nrgprf::rapl_domain>(_cd.parameters().domain_mask() & 0xff),
-        static_cast<uint8_t>(_cd.parameters().socket_mask() & 0xff), error);
-    if (error)
-        return handle_reader_error(tid, error);
-    log(log_lvl::success, "[%d] created RAPL reader", tid);
-    nrgprf::reader_gpu rdr_gpu(static_cast<uint8_t>(_cd.parameters().device_mask() & 0xff), error);
-    if (error)
-        return handle_reader_error(tid, error);
-    log(log_lvl::success, "[%d] created GPU reader", tid);
-
     // first tracer has the same tracee tgid and tid, since there is only one tracee at this point
-    tracer trc(_traps, _child, _child, entrypoint, rdr_cpu, rdr_gpu, std::launch::deferred);
+    tracer trc(_traps, _child, _child, entrypoint, _rdr_cpu, _rdr_gpu, std::launch::deferred);
     tracer_expected<gathered_results> results = trc.results();
     if (!results)
         return std::move(results.error());
 
-    profiling_results retval(std::move(rdr_cpu), std::move(rdr_gpu));
+    profiling_results retval(std::move(_rdr_cpu), std::move(_rdr_gpu));
     for (auto& [addr, execs] : results.value())
     {
         trap_set::iterator trap = _traps.find(addr);
@@ -234,12 +266,12 @@ tracer_expected<profiling_results> profiler::run()
             {
                 sr.readings.add(std::move(exec.value()));
                 log(log_lvl::success, "[%d] registered execution of section @ 0x%" PRIxPTR
-                    " (offset = 0x%" PRIxPTR ") as successful", tid, addr, addr - entrypoint);
+                    " (offset = 0x%" PRIxPTR ") as successful", _tid, addr, addr - entrypoint);
             }
             else
             {
                 log(log_lvl::error, "[%d] failed to gather results for execution of section @ 0x%"
-                    PRIxPTR " (offset = 0x%" PRIxPTR ")", tid, addr, addr - entrypoint);
+                    PRIxPTR " (offset = 0x%" PRIxPTR ")", _tid, addr, addr - entrypoint);
             }
         }
     }
--- src/profiler.hpp
+++ src/profiler.hpp
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <nrg.hpp>
+
 #include "config.hpp"
 #include "dbg.hpp"
 #include "trap.hpp"
@@ -13,18 +15,33 @@ namespace tep
 
     class profiler
     {
+    public:
+        template<typename D = dbg_line_info, typename C = config_data>
+        static cmmn::expected<profiler, tracer_error> create(pid_t child, bool pie, D&& dli, C&& cd);
+
     private:
+        pid_t _tid;
         pid_t _child;
         bool _pie;
         dbg_line_info _dli;
         config_data _cd;
         trap_set _traps;
 
+        nrgprf::reader_rapl _rdr_cpu;
+        nrgprf::reader_gpu _rdr_gpu;
+
     public:
-        profiler(pid_t child, bool pie, const dbg_line_info& dli, const config_data& cd);
-        profiler(pid_t child, bool pie, const dbg_line_info& dli, config_data&& cd);
-        profiler(pid_t child, bool pie, dbg_line_info&& dli, const config_data& cd);
-        profiler(pid_t child, bool pie, dbg_line_info&& dli, config_data&& cd);
+        profiler(pid_t child, bool pie,
+            const dbg_line_info& dli, const config_data& cd, tracer_error& err);
+
+        profiler(pid_t child, bool pie,
+            const dbg_line_info& dli, config_data&& cd, tracer_error& err);
+
+        profiler(pid_t child, bool pie,
+            dbg_line_info&& dli, const config_data& cd, tracer_error& err);
+
+        profiler(pid_t child, bool pie,
+            dbg_line_info&& dli, config_data&& cd, tracer_error& err);
 
         const dbg_line_info& debug_line_info() const;
         const config_data& config() const;
@@ -32,5 +49,15 @@ namespace tep
 
         cmmn::expected<profiling_results, tracer_error> run();
     };
+
+    template<typename D, typename C>
+    cmmn::expected<profiler, tracer_error> profiler::create(pid_t child, bool pie, D&& dli, C&& cd)
+    {
+        tracer_error err = tracer_error::success();
+        profiler prof(child, pie, std::forward<D>(dli), std::forward<C>(cd), err);
+        if (err)
+            return err;
+        return prof;
+    }
 
 }
