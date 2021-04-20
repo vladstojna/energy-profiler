68a0c9c2ea90a986dee2b5ce4d57f66cf029fb5e
vladstojna <vlad.leic@gmail.com>
2021-04-20 19:59:15 +0100

    add periodic sampler

---
+++ src/periodic_sampler.cpp
@@ -0,0 +1,176 @@
+// periodic_sampler.cpp
+
+#include "periodic_sampler.hpp"
+#include "util.hpp"
+
+#include <cassert>
+#include <unistd.h>
+
+using namespace tep;
+
+
+// begin helper functions
+
+static void handle_reader_error(pid_t tid, const char* comment, const nrgprf::error& e)
+{
+    log(log_lvl::error, "[%d] %s: error when reading counters: %s",
+        tid, comment, e.msg().c_str());
+}
+
+// end helper functions
+
+
+std::chrono::milliseconds periodic_sampler::period_max(30000);
+std::chrono::milliseconds periodic_sampler::period_default(10);
+
+
+periodic_sampler::periodic_sampler(nrgprf::execution&& exec) :
+    _future(),
+    _exec(std::move(exec)),
+    _sig(false),
+    _finished(false)
+{}
+
+periodic_sampler::periodic_sampler(nrgprf::reader_rapl& reader, nrgprf::execution&& exec,
+    periodic_sampler::complete tag,
+    const std::chrono::milliseconds& period) :
+    periodic_sampler(std::move(exec))
+{
+    (void)tag;
+    _future = std::async(std::launch::async, static_cast<nrgprf::error(periodic_sampler::*)
+        (const std::chrono::milliseconds & interval, nrgprf::reader_rapl * reader)>
+        (&periodic_sampler::evaluate),
+        this, period, &reader);
+}
+
+periodic_sampler::periodic_sampler(nrgprf::reader_rapl& reader, nrgprf::execution&& exec,
+    periodic_sampler::simple tag,
+    const std::chrono::milliseconds& period) :
+    periodic_sampler(std::move(exec))
+{
+    (void)tag;
+    _future = std::async(std::launch::async, static_cast<nrgprf::error(periodic_sampler::*)
+        (const std::chrono::milliseconds & interval, nrgprf::reader_rapl * reader)>
+        (&periodic_sampler::evaluate_simple),
+        this, period, &reader);
+}
+
+periodic_sampler::periodic_sampler(nrgprf::reader_gpu& reader, nrgprf::execution&& exec,
+    periodic_sampler::complete tag,
+    const std::chrono::milliseconds& period) :
+    periodic_sampler(std::move(exec))
+{
+    (void)tag;
+    _future = std::async(std::launch::async, static_cast<nrgprf::error(periodic_sampler::*)
+        (const std::chrono::milliseconds & interval, nrgprf::reader_gpu * reader)>
+        (&periodic_sampler::evaluate),
+        this, period, &reader);
+}
+
+periodic_sampler::~periodic_sampler() noexcept
+{
+    if (!_future.valid())
+        return;
+    _finished = true;
+    _sig.post();
+    _future.wait();
+}
+
+
+bool periodic_sampler::valid() const
+{
+    return _future.valid();
+}
+
+void periodic_sampler::start()
+{
+    _sig.post();
+}
+
+cmmn::expected<nrgprf::execution, nrgprf::error> periodic_sampler::get()
+{
+    _finished = true;
+    _sig.post();
+    nrgprf::error err = _future.get();
+    if (err)
+        return err;
+    return std::move(_exec);
+}
+
+template<typename R>
+nrgprf::error periodic_sampler::evaluate(const std::chrono::milliseconds& interval, R* reader)
+{
+    assert(reader != nullptr);
+
+    pid_t tid = gettid();
+    log(log_lvl::debug, "[%d] %s: waiting to start", tid, __func__);
+    _sig.wait();
+    do
+    {
+        nrgprf::error error = reader->read(_exec.add(nrgprf::now()));
+        if (error)
+        {
+            // wait until section or target finishes
+            handle_reader_error(tid, __func__, error);
+            _sig.wait();
+            return error;
+        }
+        _sig.wait_for(interval);
+    } while (!_finished);
+
+    nrgprf::error error = reader->read(_exec.add(nrgprf::now()));
+    if (error)
+        handle_reader_error(tid, __func__, error);
+
+    log(log_lvl::debug, "[%d] %s: finished evaluation", tid, __func__);
+    return error;
+}
+
+template<typename R>
+nrgprf::error periodic_sampler::evaluate_simple(const std::chrono::milliseconds& interval,
+    R* reader)
+{
+    assert(reader != nullptr);
+
+    pid_t tid = gettid();
+    nrgprf::sample& first = _exec.first();
+    nrgprf::sample& last = _exec.last();
+    log(log_lvl::debug, "[%d] %s: waiting to start", tid, __func__);
+    _sig.wait();
+
+    first.timepoint(nrgprf::now());
+    nrgprf::error error = reader->read(first);
+    if (error)
+    {
+        // wait until section or target finishes
+        handle_reader_error(tid, __func__, error);
+        _sig.wait();
+        return error;
+    }
+    while (!_finished)
+    {
+        _sig.wait_for(interval);
+        last.timepoint(nrgprf::now());
+        error = reader->read(last);
+        if (error)
+        {
+            handle_reader_error(tid, __func__, error);
+            _sig.wait();
+            return error;
+        }
+    };
+    log(log_lvl::debug, "[%d] %s: finished evaluation", tid, __func__);
+    return error;
+}
+
+template
+nrgprf::error periodic_sampler::evaluate(const std::chrono::milliseconds& interval,
+    nrgprf::reader_rapl* reader);
+
+template
+nrgprf::error periodic_sampler::evaluate(const std::chrono::milliseconds& interval,
+    nrgprf::reader_gpu* reader);
+
+template
+nrgprf::error periodic_sampler::evaluate_simple(const std::chrono::milliseconds& interval,
+    nrgprf::reader_rapl* reader);
---
+++ src/periodic_sampler.hpp
@@ -0,0 +1,63 @@
+// periodic_sampler.hpp
+
+#pragma once
+
+#include "signaler.hpp"
+
+#include <atomic>
+#include <future>
+
+#include <nrg.hpp>
+#include <expected.hpp>
+
+namespace tep
+{
+
+    class periodic_sampler
+    {
+    public:
+        static struct simple {} simple_sampler;
+        static struct complete {} complete_sampler;
+
+    private:
+        static std::chrono::milliseconds period_max;
+        static std::chrono::milliseconds period_default;
+
+    private:
+        std::future<nrgprf::error> _future;
+        nrgprf::execution _exec;
+        signaler _sig;
+        std::atomic_bool _finished;
+
+    public:
+        explicit periodic_sampler(nrgprf::execution&& exec = nrgprf::execution(0));
+
+        explicit periodic_sampler(nrgprf::reader_rapl& reader, nrgprf::execution&& exec,
+            complete tag,
+            const std::chrono::milliseconds& period = period_default);
+
+        explicit periodic_sampler(nrgprf::reader_rapl& reader, nrgprf::execution&& exec,
+            simple tag,
+            const std::chrono::milliseconds& period = period_max);
+
+        explicit periodic_sampler(nrgprf::reader_gpu& reader, nrgprf::execution&& exec,
+            complete tag,
+            const std::chrono::milliseconds& period = period_default);
+
+        ~periodic_sampler() noexcept;
+
+        bool valid() const;
+
+        void start();
+
+        cmmn::expected<nrgprf::execution, nrgprf::error> get();
+
+    private:
+        template<typename R>
+        nrgprf::error evaluate(const std::chrono::milliseconds& interval, R* reader);
+
+        template<typename R>
+        nrgprf::error evaluate_simple(const std::chrono::milliseconds& interval, R* reader);
+    };
+
+}
