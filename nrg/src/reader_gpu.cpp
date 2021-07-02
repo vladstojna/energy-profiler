8e2136e2325d73e9db00c77e9861523e6e7d4b3a
Vlad Stojna <vlad.leic@gmail.com>
2021-07-02 07:45:22 +0100

    PIMPL and no-op cpu reader (#41)
    * improve reader_gpu pimpl
    
    * explicit construction of bitset
    
    * add pimpl to reader_rapl
    
    * add no-op CPU reader
    
    * remove names of unused parameters
    
    * add include guards when using no-op reader

--- nrg/src/reader_gpu.cpp
+++ nrg/src/reader_gpu.cpp
@@ -95,9 +95,8 @@ lib_handle::lib_handle(lib_handle&& other) :
     lib_handle(other)
 {}
 
-lib_handle& lib_handle::operator=(const lib_handle& other)
+lib_handle& lib_handle::operator=(const lib_handle&)
 {
-    (void)other;
     nvmlReturn_t result = nvmlInit();
     if (result != NVML_SUCCESS)
         throw std::runtime_error(error_str("failed to initialise NVML", result));
@@ -135,9 +134,8 @@ lib_handle::lib_handle(lib_handle&& other) :
     lib_handle(other)
 {}
 
-lib_handle& lib_handle::operator=(const lib_handle& other)
+lib_handle& lib_handle::operator=(const lib_handle&)
 {
-    (void)other;
     rsmi_status_t result = rsmi_init(0);
     if (result != RSMI_STATUS_SUCCESS)
         throw std::runtime_error(error_str("failed to initialise ROCm SMI", result));
@@ -305,10 +303,8 @@ reader_gpu::impl::impl(device_mask dev_mask, error& ec) :
 
 #else
 
-reader_gpu::impl::impl(device_mask dev_mask, error& ec)
+reader_gpu::impl::impl(device_mask, error&)
 {
-    (void)dev_mask;
-    (void)ec;
     std::cout << fileline("No-op GPU reader\n");
 }
 
@@ -331,9 +327,8 @@ error reader_gpu::impl::read(sample& s) const
 
 #else
 
-error reader_gpu::impl::read(sample& s) const
+error reader_gpu::impl::read(sample&) const
 {
-    (void)s;
     return error::success();
 }
 
@@ -373,10 +368,8 @@ error reader_gpu::impl::read(sample& s, uint8_t ev_idx) const
 
 #else
 
-error reader_gpu::impl::read(sample& s, uint8_t ev_idx) const
+error reader_gpu::impl::read(sample&, uint8_t) const
 {
-    (void)s;
-    (void)ev_idx;
     return error::success();
 }
 
@@ -398,9 +391,8 @@ size_t reader_gpu::impl::num_events() const
 
 #else
 
-int8_t reader_gpu::impl::event_idx(uint8_t device) const
+int8_t reader_gpu::impl::event_idx(uint8_t) const
 {
-    (void)device;
     return -1;
 }
 
@@ -426,10 +418,8 @@ result<units_power> reader_gpu::impl::get_board_power(const sample& s, uint8_t d
 
 #else
 
-result<units_power> reader_gpu::impl::get_board_power(const sample& s, uint8_t dev) const
+result<units_power> reader_gpu::impl::get_board_power(const sample&, uint8_t) const
 {
-    (void)s;
-    (void)dev;
     return error(error_code::NO_EVENT);
 }
 
@@ -440,40 +430,53 @@ result<units_power> reader_gpu::impl::get_board_power(const sample& s, uint8_t d
 
 
 reader_gpu::reader_gpu(device_mask dev_mask, error& ec) :
-    _impl(std::make_shared<reader_gpu::impl>(dev_mask, ec))
+    _impl(std::make_unique<reader_gpu::impl>(dev_mask, ec))
 {}
 
 reader_gpu::reader_gpu(error& ec) :
-    reader_gpu(0xff, ec)
+    reader_gpu(device_mask(0xff), ec)
 {}
 
+reader_gpu::reader_gpu(const reader_gpu& other) :
+    _impl(std::make_unique<reader_gpu::impl>(*other.pimpl()))
+{}
 
-error reader_gpu::read(sample& s) const
+reader_gpu& reader_gpu::operator=(const reader_gpu& other)
 {
-    return _impl->read(s);
+    _impl = std::make_unique<reader_gpu::impl>(*other.pimpl());
+    return *this;
 }
 
-error reader_gpu::read(sample& s, uint8_t ev_idx) const
+reader_gpu::reader_gpu(reader_gpu&& other) = default;
+reader_gpu& reader_gpu::operator=(reader_gpu && other) = default;
+reader_gpu::~reader_gpu() = default;
+
+error reader_gpu::read(sample & s) const
 {
-    return _impl->read(s, ev_idx);
+    return pimpl()->read(s);
+}
+
+error reader_gpu::read(sample & s, uint8_t ev_idx) const
+{
+    return pimpl()->read(s, ev_idx);
 }
 
 int8_t reader_gpu::event_idx(uint8_t device) const
 {
-    return _impl->event_idx(device);
+    return pimpl()->event_idx(device);
 }
 
 size_t reader_gpu::num_events() const
 {
-    return _impl->num_events();
+    return pimpl()->num_events();
 }
 
-result<units_power> reader_gpu::get_board_power(const sample& s, uint8_t dev) const
+result<units_power> reader_gpu::get_board_power(const sample & s, uint8_t dev) const
 {
-    return _impl->get_board_power(s, dev);
+    return pimpl()->get_board_power(s, dev);
 }
 
-std::vector<reader_gpu::dev_pwr> reader_gpu::get_board_power(const sample& s) const
+std::vector<reader_gpu::dev_pwr> reader_gpu::get_board_power(const sample & s) const
 {
     std::vector<reader_gpu::dev_pwr> retval;
     for (uint32_t d = 0; d < max_devices; d++)
@@ -483,4 +486,16 @@ std::vector<reader_gpu::dev_pwr> reader_gpu::get_board_power(const sample& s) co
             retval.push_back({ d, std::move(pwr.value()) });
     }
     return retval;
+}
+
+const reader_gpu::impl* reader_gpu::pimpl() const
+{
+    assert(_impl);
+    return _impl.get();
+}
+
+reader_gpu::impl* reader_gpu::pimpl()
+{
+    assert(_impl);
+    return _impl.get();
 }
