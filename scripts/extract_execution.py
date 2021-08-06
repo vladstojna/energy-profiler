beffee6ff1e7e863c1cd13a8992839a3d2e562d4
vladstojna <vlad.leic@gmail.com>
2021-08-06 16:39:43 +0100

    support for multiple execution extraction

--- scripts/extract_execution.py
+++ scripts/extract_execution.py
@@ -5,6 +5,34 @@ import sys
 import argparse
 
 
+class intlist_or_all(argparse.Action):
+    def __init__(
+        self,
+        option_strings,
+        dest=None,
+        nargs="+",
+        **kwargs,
+    ) -> None:
+        super().__init__(option_strings, dest=dest, nargs=nargs, **kwargs)
+
+    def __call__(
+        self,
+        parser,
+        namespace,
+        values,
+        option_string,
+    ) -> None:
+        if len(values) == 1 and next(iter(values)).lower() == "all":
+            setattr(namespace, self.dest, "all")
+        else:
+            try:
+                setattr(namespace, self.dest, sorted([int(e) for e in values]))
+            except ValueError:
+                raise argparse.ArgumentError(
+                    self, "values must be base 10 integers or 'all'"
+                )
+
+
 def read_from(path):
     if not path:
         return sys.stdin
@@ -40,12 +68,13 @@ def add_arguments(parser):
     )
     parser.add_argument(
         "-e",
-        "--exec",
-        action="store",
-        help="execution index",
+        "--execs",
+        "--executions",
+        action=intlist_or_all,
+        help="execution index(es) or 'all'",
         required=False,
-        type=int,
-        default=0,
+        nargs="+",
+        default=[],
     )
     parser.add_argument(
         "-t",
@@ -58,6 +87,7 @@ def add_arguments(parser):
     )
     parser.add_argument(
         "-d",
+        "--dev",
         "--device",
         action="store",
         help="device/socket index",
@@ -109,11 +139,16 @@ def find(json_in, arg, attr, attr_compare):
         return retval
 
 
-def get_execution(execs, idx):
+def get_executions(execs, idxs):
+    if idxs == "all":
+        return execs
     try:
-        return execs[idx]
+        retval = []
+        for ix in idxs:
+            retval.append(execs[ix])
+        return retval
     except IndexError as err:
-        raise ValueError("Execution with index '{}' does not exist".format(idx))
+        raise ValueError("Execution with index '{}' does not exist".format(ix))
 
 
 def main():
@@ -129,54 +164,84 @@ def main():
 
         if not section["executions"]:
             raise ValueError("Execution is empty")
-        execution = get_execution(section["executions"], args.exec)
-        dev_key = "socket" if args.target == "cpu" else "device"
-        sensors = find(
-            execution,
-            args.device,
-            args.target,
-            dev_key,
-        )
-        if args.location and not sensors.get(args.location):
-            raise ValueError("Location {} does not exist".format(args.location))
+        if not args.execs:
+            args.execs.append(0)
+        execs = get_executions(section["executions"], args.execs)
+        if args.execs == "all":
+            args.execs = [x for x in range(len(execs))]
+        if len(execs) != len(args.execs):
+            raise AssertionError("exec count != indices requested")
 
-        sample_times = execution["sample_times"]
         sample_format = json_in["format"][args.target]
-
-        format_comment = []
-        format_comment.append("sample")
-        format_comment.append("sample_time")
-
-        value_list = [[ix, sample_times[ix]] for ix in range(len(sample_times))]
-        for loc, samples in sorted(
-            {
-                k: v
-                for k, v in sensors.items()
-                if k != dev_key
-                and v
-                and (k == args.location if args.location else True)
-            }.items()
-        ):
-            if len(value_list) != len(samples):
-                raise AssertionError("'sample_times' length != '{}' length".format(loc))
-            for dt in sample_format:
-                format_comment.append(dt + "_" + str(loc))
-
-            for lst, smp in zip(value_list, samples):
-                if len(smp) != len(sample_format):
-                    raise AssertionError("format length != sample length")
-                for value, dt in zip(smp, sample_format):
-                    lst.append(value)
+        unit_row = {k: v for k, v in json_in["units"].items()}
+        exec_row = []
+        format_row = ["sample", "sample_time"]
+        result = []
+        accum_sample = 0
+        dev_key = "socket" if args.target == "cpu" else "device"
+        for execution, idx in zip(execs, args.execs):
+            sensors = find(
+                execution,
+                args.dev,
+                args.target,
+                dev_key,
+            )
+            if args.location and sensors.get(args.location) == None:
+                raise ValueError("Location '{}' does not exist".format(args.location))
+            sample_times = execution["sample_times"]
+            exec_row.append(tuple((idx, accum_sample, len(sample_times))))
+            value_list = [
+                [accum_sample + ix, sample_times[ix]] for ix in range(len(sample_times))
+            ]
+            accum_sample += len(sample_times)
+            for loc, samples in sorted(
+                {
+                    k: v
+                    for k, v in sensors.items()
+                    if k != dev_key
+                    and v
+                    and (k == args.location if args.location else True)
+                }.items()
+            ):
+                if len(value_list) != len(samples):
+                    raise AssertionError(
+                        "'sample_times' length != '{}' length".format(loc)
+                    )
+                for dt in sample_format:
+                    entry = "{}_{}".format(dt, loc)
+                    if entry not in format_row:
+                        format_row.append(entry)
+                for lst, smp in zip(value_list, samples):
+                    if len(smp) != len(sample_format):
+                        raise AssertionError("format length != sample length")
+                    for value in smp:
+                        lst.append(value)
+            result.append(value_list)
 
         with output_to(args.output) as o:
             print(f"#{args.target}", file=o)
-            print(f"#{dev_key} {args.device}", file=o)
+            print(f"#{dev_key} {args.dev}", file=o)
             print(f"#{args.group}", file=o)
             print(f"#{args.section}", file=o)
-            print(f"#execution {args.exec}", file=o)
-            print(f"{','.join(format_comment)}", file=o)
-            for values in value_list:
-                print(",".join(str(e) for e in values), file=o)
+
+            print(
+                "#units,{}".format(
+                    ",".join("{}={}".format(k, v) for k, v in unit_row.items())
+                )
+            )
+            print(
+                "#executions,{}".format(
+                    ",".join(
+                        "i={!s}|start={!s}|size={!s}".format(i, s, sz)
+                        for i, s, sz in exec_row
+                    )
+                ),
+                file=o,
+            )
+            print(",".join(format_row), file=o)
+            for vl in result:
+                for values in vl:
+                    print(",".join(str(e) for e in values), file=o)
 
 
 if __name__ == "__main__":
