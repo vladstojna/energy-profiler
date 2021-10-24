e9a9d4f611f84be8f2be079f1cb4cf65c296c271
vladstojna <vlad.leic@gmail.com>
2021-10-24 10:42:06 +0100

    filter_duplicates_csv: add lsit of fields to consider as parameter

--- scripts/filter_duplicates_csv.py
+++ scripts/filter_duplicates_csv.py
@@ -43,10 +43,18 @@ def add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         required=False,
         default=False,
     )
+    parser.add_argument(
+        "--fields",
+        action="store",
+        help="which fields to consider when filtering a row (default: all)",
+        required=False,
+        nargs="+",
+        default=None,
+    )
     return parser
 
 
-def read_file(f) -> Tuple[csv.reader, csv.reader]:
+def read_file(f) -> Tuple[csv.reader, csv.DictReader]:
     comments = []
     rest = []
     for row in f:
@@ -54,16 +62,24 @@ def read_file(f) -> Tuple[csv.reader, csv.reader]:
             comments.append(row)
         else:
             rest.append(row)
-    return csv.reader(comments), csv.reader(rest)
+    return csv.reader(comments), csv.DictReader(rest)
 
 
-def unique_row(prev_row: Iterable, curr_row: Iterable, pred: Callable) -> bool:
+def unique_row(
+    prev_row: Iterable, curr_row: Iterable, fields: Iterable[str], pred: Callable
+) -> bool:
     if len(prev_row) != len(curr_row):
         raise AssertionError("Malformed file, rows have a different number of columns")
-    for v1, v2 in zip(prev_row, curr_row):
+    for v1, v2 in ((prev_row[f], curr_row[f]) for f in fields):
         if pred(v1, v2):
             return False
     return True
+
+
+def assert_fields(fields: Iterable[str], fieldnames: Iterable[str]):
+    for f in fields:
+        if f not in fieldnames:
+            raise AssertionError("{} is not a valid fieldname".format(f))
 
 
 def main():
@@ -76,26 +92,27 @@ def main():
     pred = (lambda _, y: not float(y)) if args.zero else (lambda x, y: x == y)
     with read_from(args.source_file) as f:
         comments, data = read_file(f)
+        if args.fields:
+            assert_fields(args.fields, data.fieldnames)
+        else:
+            args.fields = data.fieldnames
         line_num = 0
         with output_to(args.output) as of:
-            writer = csv.writer(of)
-            writer.writerows(comments)
+            csv.writer(of).writerows(comments)
             line_num += comments.line_num
-
+            dwriter = csv.DictWriter(of, data.fieldnames)
             # fieldnames row
+            dwriter.writeheader()
+            # first data row
             prev_row = next(iter(data), None)
             if prev_row is not None:
-                writer.writerow(prev_row)
-                # first data row
-                prev_row = next(iter(data), None)
-                if prev_row is not None:
-                    writer.writerow(prev_row)
-                    for row in data:
-                        if unique_row(prev_row, row, pred=pred):
-                            writer.writerow(row)
-                            prev_row = row
-                        else:
-                            log("Filtered out line", line_num + data.line_num)
+                dwriter.writerow(prev_row)
+                for row in data:
+                    if unique_row(prev_row, row, args.fields, pred=pred):
+                        dwriter.writerow(row)
+                        prev_row = row
+                    else:
+                        log("Filtered out line", line_num + data.line_num)
 
 
 if __name__ == "__main__":
