4a2109e45f47d6fbd6950fe2fc44cb1370b82961
vladstojna <vlad.leic@gmail.com>
2021-12-21 20:00:01 +0000

    mark fileline functions as local

--- nrg/src/util.hpp
+++ nrg/src/util.hpp
@@ -2,16 +2,18 @@
 
 #pragma once
 
+#include "visibility.hpp"
+
 #include <string>
 
 namespace nrgprf
 {
-
     // formats a string as 'file:line: msg' and returns formatted string
-    std::string fileline__(const char* file, int line, const std::string& msg);
-    std::string fileline__(const char* file, int line, const char* msg);
+    NRG_LOCAL std::string fileline__(
+        const char* file, int line, const std::string& msg);
+    NRG_LOCAL std::string fileline__(
+        const char* file, int line, const char* msg);
 
 #define fileline(arg) \
     fileline__(__FILE__, __LINE__, arg)
-
 }
