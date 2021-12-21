657df8eaf0440657ed55a607079192ea23014c16
vladstojna <vlad.leic@gmail.com>
2021-12-21 20:18:53 +0000

    add gpu handle type header

---
+++ nrg/src/common/gpu/gpu_handle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#if defined(GPU_NV)
+#include <nvml.h>
+#elif defined(GPU_AMD)
+#include <cstdint>
+#endif // defined(GPU_NV)
+
+namespace nrgprf
+{
+#if defined(GPU_NV)
+    using gpu_handle = nvmlDevice_t;
+#elif defined(GPU_AMD)
+    using gpu_handle = uint32_t;
+#endif // defined(GPU_NV)
+}
