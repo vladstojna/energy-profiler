fa9fe0d7488448dfcfab4ee62111fb4129ca4efa
vladstojna <vlad.leic@gmail.com>
2022-03-27 23:44:58 +0100

    add dump implementation

---
+++ src/dbg/dump.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "fwd.hpp"
+
+#include <iosfwd>
+
+namespace tep::dbg
+{
+    struct debug_dump
+    {
+        const object_info& obj_info;
+
+        explicit debug_dump(const object_info&) noexcept;
+        debug_dump(const debug_dump&) = delete;
+        debug_dump& operator=(const debug_dump&) = delete;
+    };
+
+    std::ostream& operator<<(std::ostream&, const debug_dump&);
+} // namespace tep::dbg
