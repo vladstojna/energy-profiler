9068f2664717fbf7db3c5436ec3fca4b67e7a210
vladstojna <vlad.leic@gmail.com>
2022-03-27 14:41:57 +0100

    add trap_context

---
+++ src/trap_context.cpp
@@ -0,0 +1,31 @@
+#include "trap_context.hpp"
+
+namespace tep
+{
+    bool trap_context::is_function_call() const noexcept
+    {
+        return self_->is_function_call();
+    }
+
+    uintptr_t trap_context::addr() const noexcept
+    {
+        return self_->addr();
+    }
+
+    std::string to_string(const trap_context& x)
+    {
+        return x.self_->as_string();
+    }
+
+    std::ostream& operator<<(std::ostream& os, const trap_context& x)
+    {
+        x.self_->print(os);
+        return os;
+    }
+
+    output_writer& operator<<(output_writer& os, const trap_context& x)
+    {
+        x.self_->print(os);
+        return os;
+    }
+} // namespace tep
